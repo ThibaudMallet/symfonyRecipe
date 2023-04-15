74268db5ead17efbe094c67cf474046a72b9b514
thibaud <thibaud.mallet@hotmail.fr>
2023-04-15 11:06:46 +0200

    create a command to generate an administrator

---
+++ src/Command/CreateAdministratorCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:create-administrator',
+    description: 'Create an administrator',
+)]
+class CreateAdministratorCommand extends Command
+{
+    private EntityManagerInterface $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        parent::__construct('app:create-administrator');
+        $this->manager = $manager;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('full_name', InputArgument::OPTIONAL, 'Full Name')
+            ->addArgument('email', InputArgument::OPTIONAL, 'Email')
+            ->addArgument('password', InputArgument::OPTIONAL, 'Password')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $helper = $this->getHelper('question');
+        $io = new SymfonyStyle($input, $output);
+
+        $fullName = $input->getArgument('full_name');
+        if (!$fullName) {
+            $question = new Question('Quel est le nom de l\'administrateur : ');
+            $fullName = $helper->ask($input, $output, $question);
+        }
+
+        $email = $input->getArgument('email');
+        if (!$email) {
+            $question = new Question('Quel est l\'email de l\'administrateur : ');
+            $email = $helper->ask($input, $output, $question);
+        }
+
+        $password = $input->getArgument('password');
+        if (!$password) {
+            $question = new Question('Quel est le mot de passe de l\'administrateur : ');
+            $password = $helper->ask($input, $output, $question);
+        }
+
+        $user = new User();
+
+        $user->setFullname($fullName)
+            ->setEmail($email)
+            ->setPlainPassword($password)
+            ->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        
+        $this->manager->persist($user);
+
+        $this->manager->flush();
+
+        $io->success('Votre administrateur a bien été créé !');
+
+        return Command::SUCCESS;
+    }
+}
